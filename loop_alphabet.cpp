3e909ab7b1f194d39a10af3c5ddd848bfda3f6da
Ryanmats66697 <ryanmathew66697@gmail.com>
2024-05-29 10:55:27 +0530

    update

---
+++ loop_alphabet.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int n;
+    std::cout << "Enter the value of n: ";
+    std::cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        for (char ch = 'A'; ch < 'A' + i; ++ch) {
+            std::cout << ch;
+            if (ch < 'A' + i - 1) {
+                std::cout << ", ";
+            }
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
